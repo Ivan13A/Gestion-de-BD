00084c824543767005af4ea6b6582129099c3fe0
Ivan13A <iaamayab@ufpso.edu.co>
2026-04-28 18:32:50 -0500

    Integracion de api auth, envio de email

--- src/constans/evento.const.js
+++
@@ -1,1 +0,0 @@
-export  const EVENTO_COLECCTION = "eventos"
---
+++ src/services/email.service.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer"
+const transporter = nodemailer.createTransport({
+    service: 'email',
+    auth: {
+        user: 'iaamayab@ufpso.edu.co',
+        //contraseña de aplicacion:rlxf occw wclo cchl
+        pass: 'rlxf occw wclo cchl'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+export const sendMail = async (otp)=>{
+    return await transporter.sendMail({
+        from: "Apuestas Mongo '<iaamayab@ufpso.edu.co",
+        to: 'amayabermudezivanantonio@gmail.com',
+        subject: 'Otp de inicio de seseion Apuestas Mongo',
+        html: `
+        <div>
+            <h1>Ejemplo "Apuestas mongo</h1>
+            <p>Por favor para iniciar seseion dijite el codigo OTP generadoso</p>
+            <div style ='color:white; margin:0 auto; text-aling; center; with: 14em; background:#555555, padding: 0.5em; border-radius: 5px'>
+                ${otp}
+            </div>
+        <div>´
+        `
+    })
+}
